d2cc1fe5020868baa0f8697f2b0cf5bfdd33650e
fahmifachreza <mfahmifachreza@gmail.com>
2019-05-27 14:40:19 +0700

    init commit

---
+++ env_django_ctio_db/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Volumes/WORK/INDOSAT/DASHBOARD CTIO/django_ctio_db/env_django_ctio_db/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
